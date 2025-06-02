a97f7a1e733e8dbe1dcfffcb38e01ebae843391b
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-06-02 22:44:54 +0530

    Add files via upload

---
+++ 2_If_Else/Check_Odd_Even.java
@@ -0,0 +1,9 @@
+public class Check_Odd_Even {
+  public static void main(String[] args) {
+    int x = 10;
+    if ( x % 2 == 0){
+      System.out.println(x + "is an even number");
+    }
+    else System.out.println(x + " is an odd number");
+  }
+}
---
+++ 2_If_Else/Grading_System.java
@@ -0,0 +1,16 @@
+public class Grading_System {
+  public static void main(String[] args) {
+    int marks = 87;
+    if (marks >= 90) {
+      System.out.println("Grade A");
+    } else if (marks >= 80) {
+      System.out.println("Grade B");
+    } else if (marks >= 70) {
+      System.out.println("Grade C");
+    } else if (marks >= 60) {
+      System.out.println("Grade D");
+    } else if (marks >= 50) {
+      System.out.println("Grade E");
+    }
+  }
+}
