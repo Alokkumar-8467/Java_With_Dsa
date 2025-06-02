57ba2b971e194305051242b7b035ec5d3daee5f9
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-06-02 23:15:17 +0530

    Update Print_1_to_9.java

--- 3_Loops/Print_1_to_9.java
+++ 3_Loops/Print_1_to_9.java
@@ -5,7 +5,8 @@ public class Print_1_to_9 {
     System.out.println("Enter a number");
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-    // Loop from 1 to 9
+    String name = scn.nextLine();
+    System.out.println("Hello " + name + ", you entered: " + n);
     for (int i = 0; i <= n; i++) {
       // Print the current number
       System.out.println(i);
@@ -15,3 +16,7 @@ public class Print_1_to_9 {
     System.out.println("End of program");
   }
 }
+
+// I any system if we want to take integer input then we use nextInt() method
+// If we want to take string input then we use nextLine() method  
+// If we want to take both integer and string input then we use int n = Integer.parseInt(scn.nextInt()) for Integer and then nextLine() to consume the newline character
