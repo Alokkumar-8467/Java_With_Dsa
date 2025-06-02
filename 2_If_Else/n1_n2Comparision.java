f46f281b9f452007ea3c83fd2ab676e04010b246
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-06-02 22:37:01 +0530

    Add files via upload

---
+++ 2_If_Else/n1_n2Comparision.java
@@ -0,0 +1,16 @@
+public class n1_n2Comparision{
+  public static void main(String[] args) {
+    int n1 = 15;
+    int n2 = 20;
+
+    if(n1 == n2){
+      System.out.println(n1 + " n1 is equal to n2 " + n2);
+    } 
+    else if(n1 > n2){
+      System.out.println(n1 + " n1 is greater than n2 " + n1);
+    }
+    else{
+      System.out.println(n1 + " n1 is less than n2 " + n2);
+    }
+  }
+}
