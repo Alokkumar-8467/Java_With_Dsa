4851c57a20923a3fe2f54bc4a97ca74a903f1566
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-06-02 22:34:56 +0530

    Add files via upload

---
+++ 1_Basic/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+  public static void main(String[] args ){
+    System.out.println("Hello, World!");
+    System.out.println("printing Variable = " + 6);
+    
+
+    int a = 10;
+
+    System.out.println("printing Variable a = " + a);
+
+    a = a + 5;
+
+    System.out.println("printing Variable a after increment = " + a);
+
+  }
+}
