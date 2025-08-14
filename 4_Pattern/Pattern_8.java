17127180d951f06fb166a3c6d47acc4ed80ff60c
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-08-14 14:35:09 +0530

    Add files via upload

---
+++ 4_Pattern/Pattern_8.java
@@ -0,0 +1,29 @@
+
+/*
+
+1 
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+
+import java.util.Scanner;
+
+public class Pattern_8 {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    System.out.println("Enter number of rows");
+    int row = scn.nextInt();
+
+    for(int i = 1; i <= row; i++){
+      for(int j = 1; j <= i; j++){
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+
+  }
+}
