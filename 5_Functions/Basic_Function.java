7673a136d13b2637bee8b285868c42e1e9851bb9
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-09-10 23:57:38 +0530

    Add files via upload

---
+++ 5_Functions/Basic_Function.java
@@ -0,0 +1,16 @@
+class Basic_Function{
+
+  public static int sum(int x, int y) {
+    return x+y;
+  }
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 10;
+
+    sum(a, b);
+        System.out.println(sum(a, b));
+
+
+  }
+}
