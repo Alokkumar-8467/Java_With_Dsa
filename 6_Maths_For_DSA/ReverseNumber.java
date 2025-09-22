0620f661957f72714a317f689ac93094bf8cc37d
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-09-22 09:56:42 +0530

    Add files via upload

---
+++ 6_Maths_For_DSA/ReverseNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    
+    System.out.println("Enter a Number");
+    int num = in.nextInt();
+
+    reverseNumber(num);
+    
+  }
+
+  public static void reverseNumber(int num) {
+    int revNum = 0;
+
+    while(num > 0){
+      int lastDigit = num%10;
+      revNum =  (revNum * 10 ) + ( lastDigit );
+      num = num/10;
+    }
+
+    System.out.println("Reverse of that number is " + revNum);
+
+  }
+
+}
