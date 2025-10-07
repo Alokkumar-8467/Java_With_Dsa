4f6fafaab4ff3a44e7721b9a41dd631312e4ae19
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-10-07 11:06:20 +0530

    Add files via upload

---
+++ 6_Maths_For_DSA/SumOfDigitsFrom_1_To_N.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SumOfDigitsFrom_1_To_N {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter your starting number");
+    int num = in.nextInt();
+
+    sumOfDigit(num);
+
+  }
+
+  public static void sumOfDigit (int num) {
+    
+    for(int i=1; i<=num; i++){
+      int copyOfI = i;
+
+      int sum = 0;
+
+      while(copyOfI > 0){
+        int lastDigit = copyOfI%10;
+        sum = sum + lastDigit;
+        copyOfI = copyOfI/10;
+      }
+      System.out.println("Sum of " + i + " = " + sum);
+    }
+  }
+
+  
+  
+}
