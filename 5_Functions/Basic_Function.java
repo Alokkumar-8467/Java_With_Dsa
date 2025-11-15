d04d4f1804f82cafbabab11e088d150b5033d536
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-11-15 06:18:54 +0530

    Update Basic_Function.java

--- 5_Functions/Basic_Function.java
+++ 5_Functions/Basic_Function.java
@@ -1,16 +1,25 @@
-class Basic_Function{
+class Basic_Function {
 
+  // return type has a datatype ==>> return value;
   public static int sum(int x, int y) {
-    return x+y;
+
+    int sum = x + y;
+    return sum;
+  }
+
+  // return type has void so it only print the value;
+  public static void sum1(int x, int y){
+    System.out.println(x + y);
   }
 
   public static void main(String[] args) {
     int a = 10;
     int b = 10;
 
-    sum(a, b);
-        System.out.println(sum(a, b));
+    int result = sum(a, b);
+    System.out.println(result);
 
+    sum1(a,b);
 
   }
 }
