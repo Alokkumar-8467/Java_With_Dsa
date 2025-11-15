1b06b1ccaf13e76c1fa631aaf92ff746bb921378
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-11-15 06:19:31 +0530

    Add files via upload

---
+++ 5_Functions/Product_Of_Decimal.java
@@ -0,0 +1,17 @@
+public class Product_Of_Decimal {
+
+  public static double pro(double x, double y) {
+    double products = x * y;
+    return products;
+  }
+
+  public static void main(String args[]) {
+    double a = 12.5;
+    double b = 12.5;
+
+    double result = pro(a, b);
+    System.out.println(result);
+
+  }
+
+}
