9bcf47021fd278acf4e90c999242b696cc1ce7f5
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-11-28 06:01:47 +0530

    Update Pre_Post.java

--- 5_Functions/Pre_Post.java
+++ 5_Functions/Pre_Post.java
@@ -1,19 +1,33 @@
 public class Pre_Post {
   public static void main(String[] args) {
-    
 
+    // Increment
     // PreFix Increment
     int a = 5;
-    int c = ++a;  // Pre-increment
+    int c = ++a; // Pre-increment
     System.out.println("value of a is " + a);
     System.out.println("value of c is " + c);
 
     // PostFix Increment
     int b = 5;
-    int d = b++;  //Post-increment
+    int d = b++; // Post-increment
 
     System.out.println("value of b is " + b);
     System.out.println("value of d is " + d);
+
+    // Decrement
+
+    int e = 5;
+    int f = --e; // Pre-decrement
+    System.out.println("value of e is " + e);
+    System.out.println("value of f is " + f);
+
+    // PostFix Increment
+    int g = 5;
+    int h = g--; // Post-decrement
+
+    System.out.println("value of g is " + g);
+    System.out.println("value of h is " + h);
 
   }
 }
