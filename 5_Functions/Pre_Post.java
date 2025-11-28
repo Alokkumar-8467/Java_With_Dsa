9bbab8bc6684a725433bb79ce2596f6c67e1d4a3
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-11-28 06:11:20 +0530

    Update Pre_Post.java

--- 5_Functions/Pre_Post.java
+++ 5_Functions/Pre_Post.java
@@ -29,5 +29,28 @@ public class Pre_Post {
     System.out.println("value of g is " + g);
     System.out.println("value of h is " + h);
 
+    // One question
+
+    int x = 5;
+    int y = 7;
+
+    int z = x++ + ++y + x + ++x;
+
+    /*
+     * in this program x++ so first x used then increase
+     * So, x = 5 then increase to 6.
+     * Now, ++y, so first value increase so y = 7 it increses first
+     * so it become y = 8
+     * 
+     * Now, again x, so it is use latest value because early x++ post increment so x
+     * become after increase is x = 6.
+     * So Here x = 6
+     * 
+     * Now, x = 6, again ++x again so x become x = 7
+     * So we get 5 + 8 + 6 + 7 = 26
+     */
+
+    System.out.println("value of Z is " + z);
+
   }
 }
