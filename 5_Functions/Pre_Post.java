62fd9c66f1a3a4fd2e9844c1f226f7d181d5a00b
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-11-28 05:56:41 +0530

    Add files via upload

---
+++ 5_Functions/Pre_Post.java
@@ -0,0 +1,19 @@
+public class Pre_Post {
+  public static void main(String[] args) {
+    
+
+    // PreFix Increment
+    int a = 5;
+    int c = ++a;  // Pre-increment
+    System.out.println("value of a is " + a);
+    System.out.println("value of c is " + c);
+
+    // PostFix Increment
+    int b = 5;
+    int d = b++;  //Post-increment
+
+    System.out.println("value of b is " + b);
+    System.out.println("value of d is " + d);
+
+  }
+}
