6c631078d5fd58443d9d18f213a393a695ead027
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2025-12-04 07:37:53 +0530

    Add files via upload

---
+++ 07_Array/01_Printing_Array.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    int arr[] = new int[5];
+    // by deault it contain 0.
+
+    arr[0] = 4;
+    arr[1] = 6;
+    arr[2] = 7;
+    arr[3] = 9;
+    arr[4] = 12;
+
+    for(int i = 0; i < 5; i++)
+    {
+      System.out.println(arr[i]);
+    }
+
+  }
+}
