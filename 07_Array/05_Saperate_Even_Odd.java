a0835dc926e9a251634473a5d60d2e070d5cc9f5
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-01-08 10:06:31 +0530

    Add files via upload

---
+++ 07_Array/05_Saperate_Even_Odd.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Saperate_Even_Odd {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+    int arr[] = new int[n];
+
+    for(int i =0; i<n; i++)
+    {
+      arr[i] = in.nextInt();
+    }
+
+    int countEven = 0;
+    int countOdd = 0;
+
+    for(int i = 0; i<n; i++)
+    {
+      if(arr[i]%2 == 0)
+      {
+        countEven++;
+      }
+      else{
+        countOdd++;
+      }
+    }
+
+    if(countEven > 0)
+    {
+      for(int i = 0; i<n; i++)
+      {
+        if(arr[i] % 2 == 0)
+        {
+          System.out.print(arr[i] + " ");
+        }
+      }
+      System.out.println();
+    }
+
+    if(countOdd > 0)
+    {
+      for(int i = 0; i<n; i++ )
+      {
+        if(arr[i] % 2 != 0)
+        {
+          System.out.print(arr[i] + " ");
+        }
+      }
+      System.out.println();
+    }
+
+  }
+}
